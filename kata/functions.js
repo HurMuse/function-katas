a7fa957279b8edc93bdfd7fc1985819972ee3245
HurMuse <caremplob@gmail.com>
2021-02-05 23:07:07 -0600

    oops

---
+++ kata/functions.js
@@ -0,0 +1,48 @@
+function add(a, b) {
+  z = a + b;
+  return z;
+}
+
+let kata1 = add(12, 12);
+console.log(kata1);
+document.write(kata1);
+
+function multiply(m, d) {
+  let k = 1;
+  let b = 0;
+
+  add(m, b);
+
+  while (k < d) {
+    w = add(m, z);
+    k++;
+  }
+
+  console.log(m);
+  console.log(d);
+
+  console.log(w);
+  return w;
+}
+let kata2 = multiply(5, 10);
+console.log(kata2);
+document.write(kata2);
+
+function power(x, n) {
+  let d = x;
+  let m = x;
+  let k = 1;
+
+  multiply(m, d);
+
+  while (k < m) {
+    r = add(w, z);
+    k++;
+  }
+  console.log(r);
+  return r;
+}
+
+let kata3 = power(5, 3);
+console.log(kata3);
+document.write(kata3);
